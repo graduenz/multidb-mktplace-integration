392504a288301ae95fd7e121416aa6488f455ce8
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-16 23:57:26 -0300

    feat: implemented category service

---
+++ src/common/Integration.Infrastructure/Catalog/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Integration.Application.Catalog.Dto;
+using Integration.Application.Catalog.Interfaces;
+using Integration.Infrastructure.Catalog.Persistence;
+using MapsterMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration.Infrastructure.Catalog.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly CatalogDbContext _catalogDbContext;
+    private readonly IMapper _mapper;
+
+    public CategoryService(CatalogDbContext catalogDbContext, IMapper mapper)
+    {
+        _catalogDbContext = catalogDbContext;
+        _mapper = mapper;
+    }
+    
+    public async Task<IList<CategoryDto>> GetAllCategoriesAsync() =>
+        await _catalogDbContext.Categories.Select(entity => _mapper.Map<CategoryDto>(entity)).ToListAsync();
+}
