4b6eb01e60fb50a571507ecd7b170ed32417bff5
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-16 23:21:43 -0300

    chore: added relationship collections to Category

--- src/common/Integration.Domain/Catalog/Entities/Category.cs
+++ src/common/Integration.Domain/Catalog/Entities/Category.cs
@@ -5,4 +5,8 @@ public class Category
     public Guid Id { get; set; }
     public Guid ParentId { get; set; }
     public string? Name { get; set; }
+    
+    public virtual IList<Product>? Products { get; set; }
+    public virtual IList<Category>? Children { get; set; }
+    public virtual Category? Parent { get; set; }
 }
