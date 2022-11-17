19fb009f316934afd7155b33fc6d2a789b8ccd44
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-17 08:46:14 -0300

    feat: added project's web app

---
+++ src/apps/Integration.API/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Integration.Application.Catalog.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integration.API.Controllers;
+
+[ApiController]
+[Route("v1/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _service;
+
+    public CategoriesController(ICategoryService service)
+    {
+        _service = service;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _service.GetAllCategoriesAsync();
+        return Ok(result);
+    }
+}
---
+++ src/apps/Integration.API/Program.cs
@@ -0,0 +1,27 @@
+using Integration.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
