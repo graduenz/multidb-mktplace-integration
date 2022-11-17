100cd83ca3fd9e9f5d01828cadf721527e82a376
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-17 08:27:17 -0300

    feat: implemented ProductService add/update/remove methods

--- src/common/Integration.Infrastructure/Catalog/Services/ProductService.cs
+++ src/common/Integration.Infrastructure/Catalog/Services/ProductService.cs
@@ -4,6 +4,7 @@ using Integration.Application.Catalog.Dto;
 using Integration.Application.Catalog.Interfaces;
 using Integration.Application.Interfaces;
 using Integration.Application.Models;
+using Integration.Application.Notifications;
 using Integration.Domain.Catalog.Entities;
 using Integration.Infrastructure.Catalog.Persistence;
 using MapsterMapper;
@@ -13,17 +14,22 @@ namespace Integration.Infrastructure.Catalog.Services;
 
 public class ProductService : IProductService
 {
+    private const string ProductAlreadyExists = nameof(ProductAlreadyExists);
+    private const string ProductDoesNotExist = nameof(ProductDoesNotExist);
+    
     private readonly CatalogDbContext _catalogDbContext;
     private readonly IMapper _mapper;
     private readonly IValidator<ProductDto> _validator;
+    private readonly NotificationHandler _notificationHandler;
     private readonly IMessagePublisher _publisher;
 
     public ProductService(CatalogDbContext catalogDbContext, IMapper mapper, IValidator<ProductDto> validator,
-        IMessagePublisher publisher)
+        NotificationHandler notificationHandler, IMessagePublisher publisher)
     {
         _catalogDbContext = catalogDbContext;
         _mapper = mapper;
         _validator = validator;
+        _notificationHandler = notificationHandler;
         _publisher = publisher;
     }
 
@@ -38,18 +44,55 @@ public class ProductService : IProductService
         throw new NotImplementedException();
     }
 
-    public Task AddProductAsync(ProductDto product)
+    public async Task AddProductAsync(ProductDto product)
     {
-        throw new NotImplementedException();
+        var validationResult = await _validator.ValidateAsync(product);
+        if (!validationResult.IsValid)
+        {
+            var messages = validationResult.Errors.Select(m => m.ErrorMessage);
+            _notificationHandler.AddNotification(Notification.Validation, messages);
+            return;
+        }
+
+        var entity = await _catalogDbContext.Products.FirstOrDefaultAsync(m => m.Sku == product.Sku);
+        if (entity != null)
+        {
+            _notificationHandler.AddNotification(ProductAlreadyExists, "A product with the given SKU already exists");
+            return;
+        }
+
+        await _publisher.PublishMessageAsync("integration.product.created", product);
     }
 
-    public Task UpdateProductAsync(ProductDto product)
+    public async Task UpdateProductAsync(ProductDto product)
     {
-        throw new NotImplementedException();
+        var validationResult = await _validator.ValidateAsync(product);
+        if (!validationResult.IsValid)
+        {
+            var messages = validationResult.Errors.Select(m => m.ErrorMessage);
+            _notificationHandler.AddNotification(Notification.Validation, messages);
+            return;
+        }
+
+        var entity = await _catalogDbContext.Products.FirstOrDefaultAsync(m => m.Sku == product.Sku);
+        if (entity == null)
+        {
+            _notificationHandler.AddNotification(ProductDoesNotExist, "A product with the given SKU does not exist");
+            return;
+        }
+
+        await _publisher.PublishMessageAsync("integration.product.updated", product);
     }
 
-    public Task RemoveProductAsync(string sku)
+    public async Task RemoveProductAsync(string sku)
     {
-        throw new NotImplementedException();
+        var entity = await _catalogDbContext.Products.FirstOrDefaultAsync(m => m.Sku == sku);
+        if (entity == null)
+        {
+            _notificationHandler.AddNotification(ProductDoesNotExist, "A product with the given SKU does not exist");
+            return;
+        }
+
+        await _publisher.PublishMessageAsync("integration.product.removed", new { Sku = sku });
     }
 }
