46100a2e7065c6cf98e3222ea7bf96cc2be91a9b
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-17 08:27:06 -0300

    feat: implemented simple notification pattern

---
+++ src/common/Integration.Application/Notifications/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace Integration.Application.Notifications;
+
+public class Notification
+{
+    public const string Validation = "validation";
+    
+    public Notification(string type, IReadOnlyList<string> messages)
+    {
+        Type = type;
+        Messages = messages;
+    }
+    
+    public string? Type { get; }
+    public IReadOnlyList<string>? Messages { get; }
+}
---
+++ src/common/Integration.Application/Notifications/NotificationHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Integration.Application.Notifications;
+
+public class NotificationHandler
+{
+    private readonly List<Notification> _notifications;
+    public IReadOnlyList<Notification> Notifications => _notifications;
+    public bool HasNotifications => _notifications.Any();
+    
+    public void AddNotification(string type, IEnumerable<string> messages)
+    {
+        var notification = new Notification(type, messages.ToList());
+        _notifications.Add(notification);
+    }
+    
+    public void AddNotification(string type, string message)
+    {
+        var notification = new Notification(type, new[] { message });
+        _notifications.Add(notification);
+    }
+}
