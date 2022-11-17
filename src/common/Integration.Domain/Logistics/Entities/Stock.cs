c6956ef48e0303b36371b35c3691673ff2cb8495
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-11-16 23:04:02 -0300

    feat!: changed center id to code in stock entity

--- src/common/Integration.Domain/Logistics/Entities/Stock.cs
+++ src/common/Integration.Domain/Logistics/Entities/Stock.cs
@@ -4,7 +4,7 @@ public class Stock
 {
     public Guid Id { get; set; }
     public string? Sku { get; set; }
-    public Guid CenterId { get; set; }
+    public string? CenterCode { get; set; }
     public int Quantity { get; set; }
     
     public virtual Center Center { get; set; }
